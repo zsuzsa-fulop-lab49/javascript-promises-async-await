d3b3aac3d4185e5ecde3b93d1bb34b61005f6718
Marques Woodson <marques.woodson@gmail.com>
2020-03-31 18:08:04 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -50,12 +50,8 @@ async function getBooksAndMoviesAsync() {
 }
 
 async function getBooksOrMoviesAsync() {
-  try {
     const values = await Promise.race([asyncFetchBooks(), asyncFetchMovies()]);
     return values;
-  } catch (error) {
-    console.error("Error waiting for the promise race", error);
-  }
 }
 
 getBooksAndMoviesAsync()
