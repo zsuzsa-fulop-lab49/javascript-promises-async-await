0d14a2bc176d32f4a4905cc27e9c39c44274e864
Marques Woodson <marques.woodson@gmail.com>
2020-03-31 18:10:12 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -50,8 +50,8 @@ async function getBooksAndMoviesAsync() {
 }
 
 async function getBooksOrMoviesAsync() {
-    const values = await Promise.race([asyncFetchBooks(), asyncFetchMovies()]);
-    return values;
+  const values = await Promise.race([asyncFetchBookss(), asyncFetchMovies()]);
+  return values;
 }
 
 getBooksAndMoviesAsync()
