59890be98bcab0c79e8a5761121bcd5d5cd31739
Zsuzsa <zsuzsa.fulop@lab49.com>
2022-05-04 11:40:00 +0200

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 const movies = require("./services/movies.json");
 import { fetchWithTimeout } from "./services";
-const fetchMovies = require("./services/fetchMovies");
-const fetchBooks = require("./services/fetchBooks");
 
 export const fetchMovies = () => {
   const resolveFunction = () => movies;
@@ -11,17 +9,3 @@ export const fetchMovies = () => {
 moviePromise = fetchMovies();
 
 moviePromise.then((result) => console.log(result));
-
-const getBooksAndMovies = () => {
-  return Promise.all([fetchBooks(), fetchMovies()])
-    .then(([books, movies]) => ({
-      books,
-      movies,
-    }))
-    .catch((error) => console.log("Error fetching books and movies", error));
-};
-const getBooksAndMoviesPromise = getBooksAndMovies();
-
-getBooksAndMoviesPromise.then((results) =>
-  console.log("getBooksAndMoviesPromise", results)
-);
--- src/services.js
+++ src/services.js
@@ -1,10 +1,3 @@
 export const fetchWithTimeout = (delay) => {
   return new Promise((resolve) => setTimeout(resolve, delay));
 };
-
-export const fetchBooks = () => {
-  return fetch("./data/books.json")
-    .then((response) => response.json())
-    .then((books) => books)
-    .catch((error) => console.log(error));
-};
