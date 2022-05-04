9cffc9d6bae5534134023190d94f72406865dad5
Zsuzsa <zsuzsa.fulop@lab49.com>
2022-05-04 10:21:31 +0200

    task2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-export const fetchMovies = () => {};
+const fetchWithTimeout = require("./services/fetchWithTimeout");
+
+export const fetchMovies = () => {
+  const resolveFunction = () => movies;
+  return fetchWithTimeout(1000).then(resolveFunction);
+};
+
+const moviePromise = fetchMovies();
+
+moviePromise.then((results) => {
+  console.log(results);
+});
