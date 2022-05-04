171779d75c1a46d9f4ce974cef6dabdcb6d1c9e7
Zsuzsa <zsuzsa.fulop@lab49.com>
2022-05-04 14:41:19 +0200

    .

--- src/services.js
+++ src/services.js
@@ -1,3 +1,10 @@
 export const fetchWithTimeout = (delay) => {
   return new Promise((resolve) => setTimeout(resolve, delay));
 };
+
+export const fetchBooks = () => {
+  return fetch("./data/books.json")
+    .then((response) => response.json())
+    .then((books) => books)
+    .catch((error) => console.log(error));
+};
